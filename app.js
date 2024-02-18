751f508f087702ecc3240262715b3a8278d3486f
julianunity2020 <61766920+julianunity2020@users.noreply.github.com>
2024-02-18 15:00:40 -0500

    version final juego del numero secreto

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número del uno al diez';
+
+function intentoDeUsuario(){
+    alert('CLICK DESDE EL BOTÓN');
+    }
